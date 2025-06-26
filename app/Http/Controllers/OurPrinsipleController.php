309432159ab325142f46a34822184da4d291f5f4
didepanlayar <albinmaulana@yahoo.com>
2025-06-27 00:37:12 +0700

    Feature: Create Model Migration and Controller

---
+++ app/Http/Controllers/OurPrinsipleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OurPrinsiple;
+use Illuminate\Http\Request;
+
+class OurPrinsipleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OurPrinsiple $ourPrinsiple)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OurPrinsiple $ourPrinsiple)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OurPrinsiple $ourPrinsiple)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OurPrinsiple $ourPrinsiple)
+    {
+        //
+    }
+}
